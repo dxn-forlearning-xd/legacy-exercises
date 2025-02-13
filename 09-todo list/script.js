63fa661129598f70efc483b8be90ee5a118f9e98
dxn-forlearning-xd <14mingyue@gmail.com>
2025-02-13 20:27:33 +0800

    Update script.js

--- 09-todo list/script.js
+++ 09-todo list/script.js
@@ -1,8 +1,24 @@
-const todos = [];
-function addTodo(){
-    const addEl = document.querySelector('.js-add');
-    const name = addEl.value
-    todos.push(name);
-    console.log(todos);
-    addEl.value='';
-};
+const todos = ['Make dinner', 'Wash dishes'];
+renderTodolist();
+
+function renderTodolist() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todos.length; i++) {
+    const todo = todos[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+
+  document.querySelector('.todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const addEl = document.querySelector('.js-add');
+  const name = addEl.value;
+  todos.push(name);
+  console.log(todos);
+  addEl.value = '';
+  renderTodolist();
+}
