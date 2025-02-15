0d0fe7492ee527ec8e6bc4874342a712b50bd1e1
dxn-forlearning-xd <14mingyue@gmail.com>
2025-02-15 14:48:17 +0800

    Update script.js

--- 09-todo list/script.js
+++ 09-todo list/script.js
@@ -1,15 +1,24 @@
-const todos = ['Make dinner', 'Wash dishes'];
+const todos = [
+  { name: 'make dinner', dueDate: '2025-12-22' },
+  { name: 'wash dishes', dueDate: '2025-12-21' },
+];
 renderTodolist();
 
 function renderTodolist() {
   let todoListHTML = '';
 
   for (let i = 0; i < todos.length; i++) {
-    const todo = todos[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todos[i];
+    const { name, dueDate } = todoObject;
+    const html = `<div>${name} </div> 
+    <div>${dueDate}</div> 
+        
+        <button onclick="todos.splice(${i}, 1);
+        renderTodolist();
+        "  class="delete-todo-button" >Delete</button>
+    `;
     todoListHTML += html;
   }
-  console.log(todoListHTML);
 
   document.querySelector('.todo-list').innerHTML = todoListHTML;
 }
@@ -17,8 +26,12 @@ function renderTodolist() {
 function addTodo() {
   const addEl = document.querySelector('.js-add');
   const name = addEl.value;
-  todos.push(name);
-  console.log(todos);
+  const dateEl = document.querySelector('.js-date');
+  const date = dateEl.value;
+  todos.push({
+    name: name,
+    dueDate: date,
+  });
   addEl.value = '';
   renderTodolist();
 }
